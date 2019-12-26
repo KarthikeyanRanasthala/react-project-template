f6e4e22b5a43b5a89ba5e8cccd99d7cc617368b9
albseb511 <albertsebastian511@gmail.com>
2019-12-26 10:18:49 +0530

    routes fix

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./Home.module.css";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,21 +1,27 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Link, Switch } from "react-router-dom";
 import DashboardRoutes from "./DashboardRoutes";
 import Login from "./Login";
 import Register from "./Register";
 import About from "./About";
 import Contact from "./Contact";
 import Home from "./Home";
+import NavBarPublic from "./NavbarPublic";
+import NoMatch from "./NoMatch";
 
 const Routes = () => {
   return (
     <>
-      <Route path="/" render={() => <DashboardRoutes />} />
-      <Route path="/home" render={() => <Home />} />
-      <Route path="/login" render={() => <Login />} />
-      <Route path="/register" render={() => <Register />} />
-      <Route path="/about" render={() => <About />} />
-      <Route path="/contact" render={() => <Contact />} />
+      <Route path="/" component={NavBarPublic} />
+      <Switch>
+        <Route path="/" exact render={() => <Home />} />
+        <Route path="/dash" render={() => <DashboardRoutes />} />
+        <Route path="/login" render={() => <Login />} />
+        <Route path="/register" render={() => <Register />} />
+        <Route path="/about" render={() => <About />} />
+        <Route path="/contact" render={() => <Contact />} />
+        <Route component={NoMatch} />
+      </Switch>
     </>
   );
 };
